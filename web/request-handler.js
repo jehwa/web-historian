cf79dee35339b2b84285fd520b41cb4cad8d078b
jehwa <s.jehwa@gmail.com>
2018-05-16 17:37:13 -0700

    passed first test

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,61 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+var helpers = require('./http-helpers');
+var fs = require('fs');
 // require more modules/folders here!
 
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+  var requestURL = "";
+  if (req.method === 'GET') {
+       
+    if(!requestURL.length) {
+      
+      fs.readFile(archive.paths.list, (err, data) => {
+        if(err) throw err;
+        // console.log(data.toString().includes(requestURL));
+        if(data.toString().includes(requestURL)) {
+          fs.readFile(archive.paths.archivedSites + '/' +requestURL, (err, data) => {
+            // if(err) throw err;
+            console.log(archive.paths.archivedSites + '/' +requestURL);
+            console.log(data, '============================================================================');
+            res.writeHead(200, helpers.headers);
+            res.write(data);
+            res.end();
+          })
+        }
+    
+      })
+      
+    } else {      
+      fs.readFile(archive.paths.index, (err, data) => {
+        if(err) throw err;
+        res.writeHead(200, helpers.headers);
+        res.write(data);
+        res.end(); 
+      })
+    }
+  }
+    
+
+  if (req.method === 'POST') {
+    req.on('data', function(chunk) {
+      requestURL+=chunk;
+    });
+    req.on('end', function() {
+      requestURL = requestURL.slice(8, requestURL.length-4);
+      console.log(requestURL, '<=======================================================================');
+      res.writeHead(200, helpers.headers);
+      res.end(); 
+    })     
+  } 
+  // res.end(archive.paths.list);
 };
+
+
+// exports.headers = {
+//   'access-control-allow-origin': '*',
+//   'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
+//   'access-control-allow-headers': 'content-type, accept',
+//   'access-control-max-age': 10, // Seconds.
+//   'Content-Type': 'text/html'
+// };
